22ae6ecbf645c8c1262700ab8d9b1341fc2bbbbf
Inno Irving <innoirvinge@gmail.com>
2016-03-10 02:35:17 -0800

    modified gulp file and upgraded to beta 9

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,10 @@ const config        = require('./gulp.config')();
 const PROJECT       = transpile.createProject('./tsconfig.json', { typescript: typescript });
 
 gulp.task('default', [
-    'transpile',
-    'copy'
-]);
-
-gulp.task('copy', [
+    'init.dependencies',
     'copy.assets',
-    'inject.dependencies'
+    'tslint',
+    'transpile'
 ]);
 
 gulp.task('clean', (done) => {
@@ -30,77 +27,70 @@ gulp.task('clean', (done) => {
     });
 });
 
+gulp.task('insert.dependencies', () => {
+    let angularDeps = gulp.src(config.angular2, { read: false });
+    let materializeDeps = gulp.src(config.materialize, { read: false });
+    
+    return gulp
+        .src(config.indexHTML)
+        .pipe(inject(angularDeps, { name: 'angular2' }))
+        .pipe(inject(materializeDeps, { name: 'materialize' }))
+        .pipe(gulp.dest(config.dist));
+});
+
+gulp.task('copy.dependencies', () => {
+    let dependencies = config.angular2.concat(config.materialize);
+
+    return gulp
+        .src(dependencies, { base: '.' })
+        .pipe(gulp.dest(config.dist));
+});
+
+gulp.task('init.dependencies', [
+    'copy.dependencies',
+    'insert.dependencies'
+]);
+
+gulp.task('copy.assets', () => {
+    return gulp
+        .src(config.allAssets, { 'base' : '.' })
+        .pipe(gulp.dest(config.dist));
+});
+
 gulp.task('tslint', () => {
     return gulp
         .src(config.allTS)
         .pipe(lint())
-        .pipe(lint.report('full'));
-});
-
-gulp.task('copy.assets', () => {
-    return gulp
-        .src([config.allAssets], { 'base' : '.' })
-        .pipe(gulp.dest(config.dist));
-});
-
-gulp.task('copy.index', () => {
-    return gulp
-        .src([config.indexHTML])
-        .pipe(gulp.dest(config.dist));
-});
-
-gulp.task('copy.angular2.dependencies', () => {
-    return gulp
-        .src(config.angular2, { 'base' : './node_modules' })
-        .pipe(gulp.dest(path.join(config.dist, 'node_modules')));
-});
-
-gulp.task('copy.materialize.dependencies', () => {
-    return gulp
-        .src(config.materialize, { 'base' : './node_modules' })
-        .pipe(gulp.dest(path.join(config.dist, 'node_modules')));
-});
-
-gulp.task('inject.dependencies', ['copy.angular2.dependencies', 'copy.materialize.dependencies', 'copy.index'], () => {
-    let angular2 = gulp.src(config.angular2, {read: false});
-    let materialize = gulp.src(config.materialize, {read: false});
-
-    return gulp
-        .src('./dist/index.html')
-        .pipe(inject(materialize, {name: 'materialize'}))
-        .pipe(inject(angular2, {name: 'angular2'}))
-        .pipe(gulp.dest(config.dist));
-
+        .pipe(lint.report('full'))
+            .on('error', function() {
+                this.emit('end');
+            });
 });
 
 gulp.task('transpile', () => {
-    const outDir = path.join(config.dist, 'app');
-
     return gulp
-        .src([config.allTS])
+        .src(config.allTS, { base: '.' })
         .pipe(srcmaps.init())
         .pipe(transpile(PROJECT)).js
         .pipe(srcmaps.write())
-        .pipe(gulp.dest(outDir));
+        .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('lite-server', (done) => {
-    let childProcess = exec('cd dist && lite-server');
+gulp.task('serve', ['default'], () => {
+    const allFiles = [
+        config.allTS,
+        config.allAssets,
+        config.indexHTML
+    ];
 
-    childProcess.stdout.on('on', data => {
-        console.log(data.toString());
-        done();
-    });
+    const reload = [
+        'insert.scripts',
+        'copy.dependencies',
+        'tslint',
+        'transpile',
+        'copy.assets'
+    ];
 
-    childProcess.stderr.on('on', err => {
-        console.log(err.toString());
-        done();
-    });
-});
-
-gulp.task('serve', ['tslint', 'transpile', 'copy'], () => {
-    const allFiles = ['./app/**/*.{ts,html,css}', './index.html'];
-    const tasks = ['tslint', 'transpile', 'copy.assets'];
-
-    return gulp.watch(allFiles, tasks);
+    return gulp
+        .watch(allFiles, reload);
 });
