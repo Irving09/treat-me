bfeaa78f08e12aecf6ce649bfc3075323cf8ae28
Irving <innoirvinge@gmail.com>
2016-06-03 09:41:04 -0700

    added icons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { MdToolbar } from '@angular2-material/toolbar';
 
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+
 @Component({
   moduleId: module.id,
   selector: 'tm-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css'],
-  directives: [ MdToolbar ]
+  directives: [
+  	MdToolbar,
+	MdIcon
+  ]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {}
+  constructor(mdIconRegistry: MdIconRegistry) {
+	  mdIconRegistry
+		  .addSvgIcon('thumb-up', '/app/assets/thumbup-icon.svg')
+		  .addSvgIcon('search', '/app/assets/search-icon.svg')
+		  .addSvgIcon('core', '/app/assets/core-icon-set.svg');
+  }
 
   ngOnInit() {
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { provide } from '@angular/core';
 import { enableProdMode } from '@angular/core';
+
+import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_PROVIDERS } from '@angular/router';
+
+import { MdIconRegistry } from '@angular2-material/icon/icon-registry';
+
 import { environment, TreatMeComponent } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(TreatMeComponent);
+bootstrap(TreatMeComponent, [
+	ROUTER_PROVIDERS,
+	HTTP_PROVIDERS,
+	MdIconRegistry
+]);
 
